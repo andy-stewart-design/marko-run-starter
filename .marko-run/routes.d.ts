9ecc9a13c261cfacd0ce65d6f76177ac78bca7fb
andy-stewart-design <robestewart@ebay.com>
2025-04-24 10:27:46 -0400

    initial commit

--- .marko-run/routes.d.ts
+++ .marko-run/routes.d.ts
@@ -4,7 +4,7 @@
 */
 
 import { NotHandled, NotMatched, GetPaths, PostPaths, GetablePath, GetableHref, PostablePath, PostableHref, Platform } from "@marko/run/namespace";
-import type Run from "@marko/run";
+import type * as Run from "@marko/run";
 
 
 declare module "@marko/run" {
@@ -21,19 +21,19 @@ declare module "../src/routes/_index/+page.marko" {
     export type Route = Run.Routes["/"];
     export type Context = Run.MultiRouteContext<Route> & Marko.Global;
     export type Handler = Run.HandlerLike<Route>;
+    /** @deprecated use `((context, next) => { ... }) satisfies MarkoRun.Handler` instead */
     export const route: Run.HandlerTypeFn<Route>;
   }
 }
 
 declare module "../src/routes/+layout.marko" {
-  export interface Input {
-    renderBody: Marko.Body;
-  }
+  export interface Input extends Run.LayoutInput<typeof import('../src/routes/+layout.marko')> {}
   namespace MarkoRun {
     export { NotHandled, NotMatched, GetPaths, PostPaths, GetablePath, GetableHref, PostablePath, PostableHref, Platform };
     export type Route = Run.Routes["/"];
     export type Context = Run.MultiRouteContext<Route> & Marko.Global;
     export type Handler = Run.HandlerLike<Route>;
+    /** @deprecated use `((context, next) => { ... }) satisfies MarkoRun.Handler` instead */
     export const route: Run.HandlerTypeFn<Route>;
   }
 }
--- README.md
+++ README.md
@@ -1,23 +1,6 @@
-# Thanks for checking out Marko
+# Basic Marko Run project starter
 
-# Installation
-
-```
-npx @marko/create marko-app --template basic-ts
-cd marko-app
-npm install
-npm run dev
-```
-
-## Overview
-
-This project is powered by [@marko/run](https://github.com/marko-js/run).
-
-- Run `npm run dev` to start the development server
-- Run `npm run build` to build a production-ready node.js server
-- Run `npm run preview` to run the production server
-
-## Adding Pages
-
-Pages map to the directory structure. You can add additional pages by creating files/directories under `src/routes` with `+page.marko` files.  Learn more in the [`@marko/run` docs](https://github.com/marko-js/run/#file-based-routing).
-
+- Typescript
+- eBay CoreUI
+- eBay Skin
+- Less
